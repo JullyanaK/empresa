7aa05b83bbc5e316705d00709a94dc4cf3bbf96c
JullyanaK <jullyanakaroline@gmail.com>
2022-12-27 19:26:15 -0300

    Alterações no menu envolvendo entidade Servico

--- empresa/src/main/java/ifrn/pi/empresa/controllers/ServicoController.java
+++ empresa/src/main/java/ifrn/pi/empresa/controllers/ServicoController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import ifrn.pi.empresa.models.Orcamento;
 import ifrn.pi.empresa.models.Servico;
 import ifrn.pi.empresa.repositories.ServicoRepository;
 
